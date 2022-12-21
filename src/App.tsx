20e4a9b1cbaa57e9a4d8890d35e9a974b7c56eef
Andy Mount <armount@unlimitedsystems.com>
2022-12-20 19:30:18 -0500

    Moved routing definition to JSON-Object structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
   Navigate,
-  Route,
   RouterProvider,
 } from 'react-router-dom';
 
 import './App.css';
 
 import { Instructions, Root } from './routes';
+import { TPSReport } from './routes/TPSReport';
+import { TPSReportList } from './routes/TPSReportList';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Navigate to="instructions" />} />
-      <Route path="instructions" element={<Instructions />} />
-      <Route path="tps-reports" element={<div>TPS Reports Here</div>} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        index: true,
+        element: <Navigate to="instructions" />,
+      },
+      {
+        path: 'instructions',
+        element: <Instructions />,
+      },
+      {
+        path: 'tps-reports/:id',
+        element: <TPSReport />,
+      },
+      {
+        path: 'tps-reports',
+        element: <TPSReportList />,
+      },
+    ],
+  },
+]);
 
 const App: React.FC<unknown> = () => {
   return <RouterProvider router={router} />;
